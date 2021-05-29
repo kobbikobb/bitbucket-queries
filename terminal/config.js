88c862e6f02b3638b2d583fa422e08d7976fd397
Jakob Jónasson <jakobjo@tempo.io>
2021-05-29 20:29:09 +0000

    Move terminal commants to a folder

---
+++ terminal/config.js
@@ -0,0 +1,18 @@
+const config = {
+  baseUrl: '',
+  username: '',
+  password: '',
+  target: {
+    dateFrom: '2021-04-21',
+    user: '',
+    repos: [{
+      name: '',
+      project: ''
+    },{
+      name: '',
+      project: ''
+    }]
+  }
+};
+
+module.exports = config;
